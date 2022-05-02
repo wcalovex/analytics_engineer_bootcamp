28e3be6e3e9b7fe78f510c60243340b8a8dfb195
wacalove <51934703+wacalove@users.noreply.github.com>
2022-05-02 01:26:35 -0700

    Created dimensional data warehouse layer

---
+++ ae_bootcamp/models/warehouse/dim_date.sql
@@ -0,0 +1,19 @@
+--Generate dim_date table
+select
+format_date('%F', d) as id,
+d AS full_date,
+EXTRACT(YEAR FROM d) AS year,
+EXTRACT(WEEK FROM d) AS year_weak,
+EXTRACT(DAY FROM d) AS year_day,
+EXTRACT(YEAR from d) AS fiscal_year,
+FORMAT_DATE('%Q', d) as fiscal_qtr,
+EXTRACT(MONTH FROM d) AS month, 
+FORMAT_DATE('%B', d) as month_name,
+FORMAT_DATE('%w', d) as week_day,
+FORMAT_DATE('%A', d) AS day_name,
+(CASE WHEN FORMAT_DATE('%A', d) IN ('Sunday', 'Saturday') THEN 0 ELSE 1 END) AS day_is_weekday,
+FROM (
+  SELECT 
+  *
+   FROM 
+  UNNEST(GENERATE_DATE_ARRAY('2014-01-01', '2050-01-01', INTERVAL 1 DAY)) AS d )
---
+++ ae_bootcamp/models/warehouse/dim_product.sql
@@ -0,0 +1,30 @@
+select 
+	p.id as product_id,
+	p.product_code,
+	p.product_name,
+	p.description,
+	s.company as supplier_company,
+	p.standard_cost,
+	p.list_price,
+	p.reorder_level,
+	p.target_level,
+	p.quantity_per_unit,
+	p.discontinued,
+	p.minimum_reorder_quantity,
+	p.category,
+	p.attachments,
+	current_timestamp insertion_timestamp
+from {{ref('stg_products')}} p
+left join {{ref('stg_suppliers')}} s
+on s.id = p.supplier_ids
+),
+unique_source as (
+	select *,
+		row_number() over(partition by customer_id) as row_number
+	from source
+)
+select * 
+except
+	(row_number),
+from unique_source
+where row_number = 1

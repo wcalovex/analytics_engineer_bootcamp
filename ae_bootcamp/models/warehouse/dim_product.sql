7f676f911b4eb2df1140da98b305eab03d4a36b2
wacalove <51934703+wacalove@users.noreply.github.com>
2022-05-02 02:01:29 -0700

    modified dbt_project and dim_product plus added obt layer

--- ae_bootcamp/models/warehouse/dim_product.sql
+++ ae_bootcamp/models/warehouse/dim_product.sql
@@ -1,29 +1,30 @@
-select 
-	p.id as product_id,
-	p.product_code,
-	p.product_name,
-	p.description,
-	s.company as supplier_company,
-	p.standard_cost,
-	p.list_price,
-	p.reorder_level,
-	p.target_level,
-	p.quantity_per_unit,
-	p.discontinued,
-	p.minimum_reorder_quantity,
-	p.category,
-	p.attachments,
-	current_timestamp insertion_timestamp
-from {{ref('stg_products')}} p
-left join {{ref('stg_suppliers')}} s
-on s.id = p.supplier_ids
+with source as(
+	select 
+		p.id as product_id,
+		p.product_code,
+		p.product_name,
+		p.description,
+		s.company as supplier_company,
+		p.standard_cost,
+		p.list_price,
+		p.reorder_level,
+		p.target_level,
+		p.quantity_per_unit,
+		p.discontinued,
+		p.minimum_reorder_quantity,
+		p.category,
+		p.attachments,
+		current_timestamp insertion_timestamp
+	from {{ref('stg_products')}} p
+	left join {{ref('stg_suppliers')}} s
+	on s.id = p.supplier_id
 ),
 unique_source as (
 	select *,
-		row_number() over(partition by customer_id) as row_number
+		row_number() over(partition by product_id) as row_number
 	from source
 )
-select * 
+select *
 except
 	(row_number),
 from unique_source
